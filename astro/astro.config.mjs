f844f550d369a713ac47be5b20278092f7d880ca
ricard-gi <153858144+ricard-gi@users.noreply.github.com>
2024-03-24 23:09:55 +0100

    middleware

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -7,7 +7,7 @@ import node from '@astrojs/node';
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: 'standalone',
+    mode: 'middleware',
   }),
   integrations: [tailwind()]
 });
